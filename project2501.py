fab2217bc9ed27d6a705d32ff654a891d71bf7b8
psycedelicAI <130298635+psycedelicAI@users.noreply.github.com>
2026-08-26 14:43:06 +0200

    Create project2501.py

---
+++ project2501.py
@@ -0,0 +1,14 @@
+from typing import Final
+
+BLOCKED_DOMAINS: Final[set[str]] = {
+"test-blocked.invalid",
+}
+
+BLOCKED_ALIASES: Final[set[str]] = {
+}
+
+HIGH_CONFIDENCE_TERMS: Final[set[str]] = {
+}
+
+BLOCKED_TELEGRAM_PATTERNS: Final[set[str]] = {
+}
